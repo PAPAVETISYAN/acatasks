e49512034aa7a92dcb9017084630ab5c5c3179ec
PapAvetisyan <avetisyanpap@Gmail.com>
2021-04-26 17:51:56 +0400

    Add solution

---
+++ src/Circle.java
@@ -0,0 +1,11 @@
+public class Circle {
+
+    public static void main(String[] args) {
+        int radius=3;
+        double p=3.14;
+        double area=radius*radius*p;
+        System.out.println("Area is = " + area);
+        double perimete=2*p*radius;
+        System.out.println("Perimete =" + perimete);
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,10 @@
+public class Triangle {
+
+    public static void main(String[] args) {
+        int a=3,b=4,c=5;
+        int area=a*b/2;
+        int perimete=a+b+c;
+        System.out.println("Area is="+area);
+        System.out.println("Perimete is="+perimete);
+    }
+}
